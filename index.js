1ff2c1c125a033b60a605335725bfe9582bba94f
Griffins Shem Ondeyo <griffinsshem254@gmail.com>
2025-06-03 07:28:20 +0300

    add phase-1-review-strings-lab

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code in this file!
+const currentUser = "Griffins Ondeyo";
+const welcomeMessage = "Welcome to Flatbook, " + currentUser + "!";
+
+const upperCaseCurrentUser = currentUser.toUpperCase();
+const excitedWelcomeMessage = "WELCOME TO FLATBOOK, " + upperCaseCurrentUser + "!";
+
+// const firstInitial = currentUser[0];
+// const shortGreeting = "Welcome, " + firstInitial + "!";
+
+const firstInitial = currentUser.slice(0, 1);
+const shortGreeting = "Welcome, " + firstInitial + "!";
